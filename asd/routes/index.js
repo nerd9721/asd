0a6ca39a3a4a26c3b24764e8d68a7e6b905c4a6e
nerd9721 <nerd9721@gmail.com>
2014-08-07 19:15:07 +0900

    test2

--- asd/routes/index.js
+++ asd/routes/index.js
@@ -6,5 +6,5 @@ exports.index = function(req, res)
   //var repos = _git_core.get_repos();
 
   //res.render('index', { title: 'Express', doneTasks : v, num:Math.random()});
-  res.render('index', { title : 'helo'});
+  res.render('index', { title : 'helo', jade_github_repos: ['ewrwer', 'werewr']});
 };
--- asd/routes/user.js
+++ asd/routes/user.js
@@ -8,7 +8,9 @@ exports.list = function(req, res){
   //JSONObject = { 'name':'kyunghun' };
   console.log('calsdlasdssad@');
 
-  var param0 = ['kyunghun', 'plus'];
+  //var param0 = ['kyunghun', 'plus'];
+  //var param0 = '[{"id":"1","name":"test1"},{"id":"2","name":"test2"},{"id":"3","name":"test3"},{"id":"4","name":"test4"},{"id":"5","name":"test5"}]';
+  var param0 = '["iopoipoip", "iopoipoiop"]';
   
   var _json = JSON.stringify({
     keyword : param0
